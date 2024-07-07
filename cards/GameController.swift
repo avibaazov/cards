f30231fbd11b9da5a959277734842317d74ce620
avibaazov <avibaazov2@gmail.com>
2024-07-07 21:21:39 +0300

    i add new screen and deck

---
+++ cards/GameController.swift
@@ -0,0 +1,11 @@
+//
+//  GameController.swift
+//  cards
+//
+//  Created by Student15 on 07/07/2024.
+//
+
+import UIKit
+class GameController: UIViewController{
+    
+}
--- cards/ViewController.swift
+++ cards/ViewController.swift
@@ -4,19 +4,26 @@
 //
 //  Created by Student15 on 07/07/2024.
 //
-
 import UIKit
+
+typealias CLLocationDegrees = Double
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var addName: UIButton!
+    @IBOutlet var addName: UIButton!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var sideLabel: UILabel! // New label for indicating the side
+
+    let dummyLatitude: CLLocationDegrees = 34.0 // Example latitude for testing
+    let midpointLatitude: CLLocationDegrees = 34.817549168324334
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("view did load")
+        
+        print("View did load")
+        
         // Check if a name is already saved
         if let savedName = UserDefaults.standard.string(forKey: "userName") {
             // User has already entered their name
@@ -25,29 +32,49 @@ class ViewController: UIViewController {
             nameField.isHidden = true
             addName.isHidden = true
             nameLabel.isHidden = false
+            startButton.isHidden = true
+            sideLabel.isHidden = true
+            determineUserSide(dummyLatitude: dummyLatitude)
         } else {
             // User needs to enter their name
+            print("No saved name found")
             nameField.isHidden = false
             addName.isHidden = false
             nameLabel.isHidden = true
+            startButton.isHidden = true
+            sideLabel.isHidden = true
         }
+    }
+
+    func determineUserSide(dummyLatitude: CLLocationDegrees) {
+        if dummyLatitude > midpointLatitude {
+            print("You are on the East Side")
+            sideLabel.text = "You will play on the East Side"
+        } else {
+            print("You are on the West Side")
+            sideLabel.text = "You will play on the West Side"
+        }
+        sideLabel.isHidden = false
+        startButton.setTitle("Start", for: .normal)
+        startButton.isHidden = false
     }
 
     @IBAction func addNameTapped(_ sender: UIButton) {
         if let name = nameField.text, !name.isEmpty {
             // Save the name
             UserDefaults.standard.set(name, forKey: "userName")
+            print("Name saved: \(name)")
             
             // Update the UI
             nameLabel.text = "Hi \(name)"
             nameField.isHidden = true
             addName.isHidden = true
             nameLabel.isHidden = false
+            determineUserSide(dummyLatitude: dummyLatitude)
+        } else {
+            print("Name field is empty")
         }
     }
     
-   // @IBAction func startButtonTapped(_ sender: UIButton) {
-        // Navigate to the next screen
-     //   performSegue(withIdentifier: "showGameScreen", sender: nil)
-   // }
+  
 }
