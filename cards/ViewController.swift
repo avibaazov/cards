3034e4aa59f52bc54ab98a2c6663f62ef3636937
avibaazov <avibaazov2@gmail.com>
2024-07-08 22:28:30 +0300

    location

--- cards/ViewController.swift
+++ cards/ViewController.swift
@@ -3,23 +3,22 @@
 //  cards
 //
 //  Created by Student15 on 07/07/2024.
-//
 import UIKit
+import CoreLocation
 
-typealias CLLocationDegrees = Double
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-class ViewController: UIViewController {
-    
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet var addName: UIButton!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var sideLabel: UILabel! // New label for indicating the side
+
+    let locationManager = CLLocationManager()
+    let midpointLatitude: Double = 34.817549168324334
     var userName: String = ""
     var userSide: String = ""
-    let dummyLatitude: CLLocationDegrees = 34.0 // Example latitude for testing
-    let midpointLatitude: CLLocationDegrees = 34.817549168324334
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,15 +27,14 @@ class ViewController: UIViewController {
         // Check if a name is already saved
         if let savedName = UserDefaults.standard.string(forKey: "userName") {
             // User has already entered their name
-            userName=savedName
-            print("Saved name found: \(savedName)")
+            userName = savedName
             nameLabel.text = "Hi \(savedName)"
             nameField.isHidden = true
             addName.isHidden = true
             nameLabel.isHidden = false
             startButton.isHidden = true
             sideLabel.isHidden = true
-            determineUserSide(dummyLatitude: dummyLatitude)
+            determineUserSideBasedOnLocation()
         } else {
             // User needs to enter their name
             print("No saved name found")
@@ -47,35 +45,45 @@ class ViewController: UIViewController {
             sideLabel.isHidden = true
         }
     }
-    
-    func determineUserSide(dummyLatitude: CLLocationDegrees) {
-        if dummyLatitude > midpointLatitude {
-            print("You are on the East Side")
-            sideLabel.text = "You will play on the East Side"
-            userSide = "right"
-        } else {
-            print("You are on the West Side")
-            sideLabel.text = "You will play on the West Side"
-            userSide = "left"
-            
-        }
-        sideLabel.isHidden = false
-        startButton.setTitle("Start", for: .normal)
-        startButton.isHidden = false
+
+    func determineUserSideBasedOnLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-    
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            locationManager.stopUpdatingLocation()
+            let latitude = location.coordinate.latitude
+            if latitude > midpointLatitude {
+                print("You are on the East Side")
+                sideLabel.text = "You will play on the East Side"
+                userSide = "right"
+            } else {
+                print("You are on the West Side")
+                sideLabel.text = "You will play on the West Side"
+                userSide = "left"
+            }
+            sideLabel.isHidden = false
+            startButton.setTitle("Start", for: .normal)
+            startButton.isHidden = false
+        }
+    }
+
     @IBAction func addNameTapped(_ sender: UIButton) {
         if let name = nameField.text, !name.isEmpty {
             // Save the name
             UserDefaults.standard.set(name, forKey: "userName")
+            userName = name
             print("Name saved: \(name)")
-            userName=name
+            
             // Update the UI
             nameLabel.text = "Hi \(name)"
             nameField.isHidden = true
             addName.isHidden = true
             nameLabel.isHidden = false
-            determineUserSide(dummyLatitude: dummyLatitude)
+            determineUserSideBasedOnLocation()
         } else {
             print("Name field is empty")
         }
@@ -92,5 +100,4 @@ class ViewController: UIViewController {
             navigationController?.pushViewController(gameVC, animated: true)
         }
     }
-    
 }
